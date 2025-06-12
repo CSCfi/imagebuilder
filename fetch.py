5c16566de93dcbba4275c4b984d8507a96207d92
Luka Jääskeläinen <luka.jaaskelainen@csc.fi>
2025-06-12 16:54:34 +0300

    Improve error handling and structure the project better Handle different kind of exceptions that can occur Now invalid urls will not crash the script
    Make the code easier to understand

--- fetch.py
+++ fetch.py
@@ -30,25 +30,160 @@ def download_file(url: str, filename: str) -> bool:
                     f.write(chunk)
 
         return True
+
+    except requests.exceptions.HTTPError:
+        print(f"HTTP error while downloading {filename}")
+
+    except requests.exceptions.ReadTimeout:
+        print(f"Download for {filename} timed out")
+
     except requests.exceptions.ConnectionError:
         print(f"Unable to download {filename}")
-        return False
+
+
+    return False
 
 
 
-def fetch_checksum(url: str, filename: str, fallback: str | None) -> str | None:
+
+
+
+
+def validate_checksum(url: str, filename: str, image_name: str) -> str | None:
     """
-    Fetches a checksum file from url and finds a match for filename
+    Validates checksums to check if an image requires updating
     """
+    old_checksum = None
+    if os.path.exists("checksums/"+filename+"_CHECKSUM"):
+        with open("checksums/"+filename+"_CHECKSUM","r", encoding="utf-8") as f:
+            old_checksum = f.read()
+
+
+    error = False
+    new_checksum = old_checksum
     try:
-        return next((
+        new_checksum = next((
                 line
                 for line in requests.get(url, timeout=5).text.split('\n')
                     if filename in line
-            ), fallback)
+            ), old_checksum)
+
+    except requests.exceptions.HTTPError:
+        error = True
+        print(f"HTTP error while downloading {image_name}")
+
+    except requests.exceptions.ReadTimeout:
+        error = True
+        print(f"Fetching the checksum for {image_name} timed out")
+
     except requests.exceptions.ConnectionError:
-        print(f"Unable to fetch new checksum for {filename}")
-        return fallback
+        error = True
+        print(f"Unable to fetch new checksum for {image_name}")
+
+
+
+
+    if error:
+        print(f"An error occured validating {image_name}")
+        return None
+
+    if new_checksum == old_checksum:
+        print(f"{image_name} already up to date")
+        return None
+
+
+
+    return new_checksum
+
+
+
+
+def test_image(conn: openstack.connection.Connection, image: any) -> bool:
+    """
+    Test the newly created image by creating a test server
+    """
+
+    test_server = conn.create_server(
+        name=image.name+"_TESTSERVER",
+        image=image,
+        flavor="standard.tiny",
+        wait=True,
+        auto_ip=False,
+        network=os.getenv("NETWORK"),
+        timeout=360
+    )
+
+    found_test_server = conn.get_server_by_id(test_server.id)
+    if found_test_server["status"] == "ERROR":
+        print(f"Server {test_server.id} failed to start with image {image.id}!")
+        return False
+
+    conn.delete_server(test_server.id)
+
+
+
+
+    return True
+
+
+def cleanup_files(filename: str) -> None:
+    """
+    Delete image files that the program creates.
+    """
+
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+    try:
+        os.remove(filename+".raw")
+    except OSError:
+        pass
+
+
+
+
+def create_image(conn: openstack.connection.Connection, version: any, filename: str) -> any:
+    """
+    Creates an image
+    """
+
+    # Download image
+    if not download_file(version["image_url"],filename):
+        cleanup_files(filename)
+        return None
+
+
+
+    # convert to raw
+    os.system(f"qemu-img convert -f qcow2 -O raw {filename} {filename}.raw")
+
+    new_image = conn.create_image(
+        name=version["image_name"],
+        filename=filename+".raw",
+        wait=True,
+        visibility="private",
+        allow_duplicates=True,
+        disk_format="raw",
+        container_format="bare",
+        properties={
+            "description":"To find out which user to login with: ssh in as root.",
+            "os_distro":version["distro"]
+        }
+    )
+
+
+    cleanup_files(filename)
+
+    # Test image
+    if not test_image(conn, new_image):
+        return None
+
+
+
+
+    return new_image
 
 
 
@@ -72,67 +207,20 @@ def main() -> None:
     for version in input_data["new"]:
         filename = version["image_url"].split("/")[-1]
 
-        old_checksum = None
-        if os.path.exists("checksums/"+filename+"_CHECKSUM"):
-            with open("checksums/"+filename+"_CHECKSUM","r", encoding="utf-8") as f:
-                old_checksum = f.read()
+        new_checksum = validate_checksum(version["checksum_url"], filename, version["image_name"])
 
-
-        new_checksum = fetch_checksum(version["checksum_url"], filename, old_checksum)
-
-
-        if new_checksum == old_checksum:
-            print(f"{version["image_name"]} already up to date")
+        if new_checksum is None:
             continue
+
+
 
         print(f"Downloading {version["image_name"]}")
 
+        new_image = create_image(conn, version, filename)
 
-
-
-        # Download image
-        if not download_file(version["image_url"],filename):
+        if new_image is None:
+            print("An error occured while creating the image")
             continue
-
-
-
-        # convert to raw
-        os.system(f"qemu-img convert -f qcow2 -O raw {filename} {filename}.raw")
-
-        new_image = conn.create_image(
-            name=version["image_name"],
-            filename=filename+".raw",
-            wait=True,
-            visibility="private",
-            allow_duplicates=True,
-            disk_format="raw",
-            container_format="bare",
-            properties={
-                "description":"To find out which user to login with: ssh in as root.",
-                "os_distro":version["distro"]
-            }
-        )
-
-
-
-        # Test image
-        test_server = conn.create_server(
-            name=version["image_name"]+"_TESTSERVER",
-            image=new_image,
-            flavor="standard.tiny",
-            wait=True,
-            auto_ip=False,
-            network=os.getenv("NETWORK"),
-            timeout=360
-        )
-
-        found_test_server = conn.get_server_by_id(test_server.id)
-        if found_test_server["status"] == "ERROR":
-            print(f"Server {test_server.id} failed to start with image {new_image.id}!")
-            continue
-
-        conn.delete_server(test_server.id)
-
 
 
 
@@ -143,7 +231,7 @@ def main() -> None:
 
             # Check if image is unused
             if (next(conn.compute.servers(image=img.id), None) is None and
-                next(conn.block_storage.volumes(image_id=img.id),None) is None):
+                next(conn.block_storage.volumes(image_id=img.id), None) is None):
                 # not used by any server. good to delete?
                 conn.delete_image(img.id)
             else:
@@ -151,11 +239,10 @@ def main() -> None:
                 conn.image.update_image(img.id, visibility="community")
 
 
+        # Everything is ok! Set visibility to what it should be
         conn.image.update_image(new_image.id, visibility=version["visibility"])
 
 
-        os.remove(filename) # Cleanup
-        os.remove(filename+".raw")
 
         with open("checksums/"+filename+"_CHECKSUM","w", encoding="utf-8") as f:
             f.write(new_checksum)
