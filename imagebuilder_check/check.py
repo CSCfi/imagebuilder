20a3ae62a2cf958cb6c6410561865446047f04f0
Luka Jääskeläinen <luka.jaaskelainen@csc.fi>
2025-07-22 16:25:09 +0300

    Add checking for empty log file
    This most likely happens when logrotate rotates the log so it's a
    warning. The issue fixes itself within a day or so.

--- imagebuilder_check/check.py
+++ imagebuilder_check/check.py
@@ -27,9 +27,19 @@ def get_run_data(filename: str, cloud: str) -> dict:
     json_data = None
     try:
         with open(filename, "r", encoding="utf-8") as f:
-            json_data = json.load(f)
+            content = f.read()
+
+            if not content:
+                print("The given log file is empty and therefore cannot be analyzed")
+                print("This is most likely caused by the log being rotated and it will fix itself")
+                sys.exit(NAGIOS_STATE_WARNING)
+
+            json_data = json.loads(content)
     except IOError as e:
         print(f"Failed to open file: {e}")
+        sys.exit(NAGIOS_STATE_CRITICAL)
+    except json.decoder.JSONDecodeError as e:
+        print(f"Log json could not be decoded: {e}")
         sys.exit(NAGIOS_STATE_CRITICAL)
 
     for run in reversed(json_data):
